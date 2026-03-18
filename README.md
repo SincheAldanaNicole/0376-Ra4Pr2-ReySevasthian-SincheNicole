3d030af70159ec941873d835a3e453dbb0ca0055
Nicole <nsinche25@ilb.cat>
2026-03-18 13:25:55 +0100

    Añadi el markdown

---
+++ README.md
@@ -0,0 +1,14 @@
+### Bateria de preguntes obertes sobre Git i GitHub
+
+## Alumnes: Sevasthian Rey i Nicole Sinche.
+
+---
+
+### 1.Reconeix la utilitat de les aplicacions d’ofimàtica web
+
+- **Explica per què GitHub es pot considerar una aplicació web de treball col·laboratiu.**
+
+- **Quins avantatges té utilitzar GitHub en lloc de guardar els fitxers d’un projecte només en un ordinador local?**
+- **
+-
+-
---
+++ javaScript.js
@@ -0,0 +1,11 @@
+/* Estudiant A: Gestor de la Terminal i UI (feature-ui-logic)
+
+La teva missió és que el sistema "parli" amb l'usuari i reculli les dades.
+
+1.-Generació de Selects: Crear un bucle que ompli els 4 desplegables amb números del 0 al 9.
+
+2.-Funció logTerminal(missatge, tipus): Crear una funció que rebi un text i el tipus (èxit, error o normal) i l'imprimeixi a la zona de terminal del HTML amb un efecte de "línia de comanda".
+
+3.-Gestor d'Esdeveniments: Escoltar el clic del botó "Executar Codi", recollir els valors dels 4 selects i cridar a la lògica de comparació.
+
+4.-Control de Rondes: Actualitzar el comptador visual de rondes restants a cada intent.*/
