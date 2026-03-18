eabd30d1adc588826a0f21b55fe16988a2f70179
ReyGomezSevasthian <jrey25@ilb.cat>
2026-03-18 13:28:57 +0100

    Creación de Js. sevas

---
+++ script.js
@@ -0,0 +1,8 @@
+//Parte de Sevasthian Rey. 
+
+/*1. Crear un array de 4 números aleatorios (0-9)
+al momento de cargar la página*/
+
+/*2. Crear una función que reciba el intento del 
+usuario y que retorne un array de pistas (1, Ø, x)*/
+
