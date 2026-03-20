bcfef9dde3e9706cfb79e72395acb6c9a9390d5f
SincheAldanaNicole <nsinche25@ilb.cat>
2026-03-20 23:27:24 +0100

    Añadi el bucle de los selects

--- javaScript.js
+++ javaScript.js
@@ -9,3 +9,29 @@ let intentsRestants = MAX_INTENTS;
 // El código secreto lo generará el Estudiant B.
 // De momento lo dejamos vacío para que no pete la página.
 let codiSecret = [];
+
+// Rellenamos los 4 desplegables con números del 0 al 9.
+
+// Cogemos todos los elementos <select> que tienen la clase "code-input"
+// querySelectorAll nos devuelve una lista con los 4 selects
+const selects = document.querySelectorAll('.code-input');
+ 
+// Recorremos cada select uno por uno
+selects.forEach(function(select) {
+ 
+    // Bucle del 0 al 9 para crear las opciones numéricas
+    for (let i = 0; i <= 9; i++) {
+ 
+        // Creamos un elemento <option> nuevo en el DOM
+        const opcio = document.createElement('option');
+ 
+        // El valor interno de la opción será el número (ej: 0, 1, 2...)
+        opcio.value = i;
+ 
+        // El texto que ve el usuario en el desplegable también es el número
+        opcio.textContent = i;
+ 
+        // Añadimos la opción dentro del select correspondiente
+        select.appendChild(opcio);
+    }
+});
