dcbc04e07703e39f14752fc32c9073d1759180c5
Sevasthian Rey <jrey25@ilb.cat>
2026-03-20 22:05:05 +0100

    Primer paso js y pequeño cambio del html para comprobar

--- script.js
+++ script.js
@@ -2,7 +2,12 @@
 
 /*1. Crear un array de 4 números aleatorios (0-9)
 al momento de cargar la página*/
+const codigoSecreto = []
 
+for (let i = 0; i <4; i++){
+    codigoSecreto.push(Math.round(Math.random()*10))
+}
+console.log(codigoSecreto)
 /*2. Crear una función que reciba el intento del 
 usuario y que retorne un array de pistas (1, Ø, x)*/
 
